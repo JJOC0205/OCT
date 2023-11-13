310fd0fa59ccab735cf8a59559ccf627f8226932
Jon O'Connell <jjoc0205@gmail.com>
2023-11-13 14:31:54 -0500

    Trying ot set up migrations

---
+++ Shared/DTO/ZipCodeDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace OCTOBER.Shared.DTO
+{
+    public class ZipCodeDTO
+    {
+        [StringLength(5)]
+        public string Zip { get; set; } = null!;
+        [StringLength(25)]
+        public string City { get; set; } = null!;
+        [StringLength(2)]
+        public char State { get; set; }
+        [StringLength(30)]
+        public string CreatedBy { get; set; } = null!;
+        public DateTime CreatedDate { get; set; }
+        [StringLength(30)]
+        [Unicode(false)]
+        public string ModifiedBy { get; set; } = null!;
+        public DateTime ModifiedDate { get; set; }
+
+
+    }
+}
